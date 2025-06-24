038a40883c54b93eb057c238e78e0ed9e083b67a
SoumyaMakkar <soumyamakkar123@gmail.com>
2025-06-24 15:07:02 +0530

    gh

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -5,7 +5,7 @@ const { protect } = require('../middlewares/authMiddleware');
 
 // Single endpoint for all menu item queries with filter in query params
 router.get('/menu-items', protect, homeController.getMenuItems);
-
+router.get('/popular-dishes', protect, homeController.getPopularDishes);
 // Keep the chef/kitchen endpoints separate since they're different entities
 router.get('/top-kitchens', protect, homeController.getTopHomeKitchens);
 router.get('/popular-chefs', protect, homeController.getPopularChefs);
